d7f5ca5fed7179fe04e0ee12b2ac06a55a2c3e1a
Sou <smonster12710@gmail.com>
2026-05-13 11:15:07 +0700

    image_detail

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
 
-document.querySelectorAll('.card').forEach(card => {
+    document.querySelectorAll('.post-clickable').forEach(card => {
+
         card.addEventListener('click', function (e) {
 
-            // ❌ Nếu click vào button, form, link thì bỏ qua
-            if (
-                e.target.closest('button') ||
-                e.target.closest('form') ||
-                e.target.closest('a')
-            ) {
+            // nếu click vào vùng bị loại trừ
+            if (e.target.closest('.no-post-modal')) {
                 return;
             }
 
-            // ✅ chuyển trang
-            window.location.href = this.dataset.url;
+            const postId = this.dataset.postId;
+
+            const modalEl = document.getElementById(
+                `instagramModal${postId}`
+            );
+
+            if (modalEl) {
+                const modal = new bootstrap.Modal(modalEl);
+                modal.show();
+            }
+
         });
+
+    });
+
 });
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -380,5 +380,6 @@ $avatar = $user && $user->avatar_url
 </script>
 <script src="{{ asset('js/search.js') }}"></script>
 <script src="{{ asset('js/search_post.js') }}"></script>
+<script src="{{ asset('js/index.js') }}"></script>
 
 </html>
