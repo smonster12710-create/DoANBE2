e793804e3bc18b73729459c543e3be23bad8444c
NguyenVanTuan <nvantuan2k6@gmail.com>
2026-05-28 16:52:10 +0700

    fix bug

--- app/Events/UserFollowed.php
+++ app/Events/UserFollowed.php
@@ -1,4 +1,4 @@
-<?
+<?php
 namespace App\Events;
 
 use Illuminate\Broadcasting\InteractsWithSockets;
--- database/migrations/2026_05_27_054904_modify_type_column_in_notifications_table.php
+++ database/migrations/2026_05_27_054904_modify_type_column_in_notifications_table.php
@@ -1,4 +1,5 @@
-<? use Illuminate\Database\Migrations\Migration;
+<?php
+use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
 return new class extends Migration {
